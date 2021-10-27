c2cd7ad83bbbad95270fb118ef875552334d6a89
zoomcityzens <zoomcitydigital@gmail.com>
2021-10-26 22:33:25 -0400

    update

--- jfkcompack_backend/jfkcompack_backend/settings.py
+++ jfkcompack_backend/jfkcompack_backend/settings.py
@@ -29,7 +29,7 @@ with open(ENV_PATH, 'r') as file:
 SECRET_KEY = env.get('API_ID')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = F
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost'
